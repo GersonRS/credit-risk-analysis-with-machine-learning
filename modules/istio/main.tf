992ef517a30633a96d29b21a79f70844f6646c0d
GersonRS <gersonrodriguessantos8@gmail.com>
2024-02-20 19:00:55 -0300

    Fix: Apply fix istio module

--- modules/istio/main.tf
+++ modules/istio/main.tf
@@ -34,48 +34,16 @@ resource "argocd_project" "this" {
   }
 }
 
-# resource "argocd_project" "gateway" {
-#   count = var.argocd_project == null ? 1 : 0
-
-#   metadata {
-#     name      = var.destination_cluster != "in-cluster" ? "istio-gateway-${var.destination_cluster}" : "istio-gateway"
-#     namespace = var.argocd_namespace
-#     annotations = {
-#       "modern-gitops-stack.io/argocd_namespace" = var.argocd_namespace
-#     }
-#   }
-
-#   spec {
-#     description  = "istio application project for cluster ${var.destination_cluster}"
-#     source_repos = [var.project_source_repo]
-
-
-#     destination {
-#       name      = var.destination_cluster
-#       namespace = "istio-ingress"
-#     }
-
-#     orphaned_resources {
-#       warn = true
-#     }
-
-#     cluster_resource_whitelist {
-#       group = "*"
-#       kind  = "*"
-#     }
-#   }
-# }
-
 data "utils_deep_merge_yaml" "values" {
   input = [for i in concat(local.helm_values, var.helm_values) : yamlencode(i)]
 }
 
-resource "argocd_application" "this" {
+resource "argocd_application" "base" {
   metadata {
-    name      = var.destination_cluster != "in-cluster" ? "istio-${var.destination_cluster}" : "istio"
+    name      = var.destination_cluster != "in-cluster" ? "istio-base-${var.destination_cluster}" : "istio-base"
     namespace = var.argocd_namespace
     labels = merge({
-      "application" = "istio"
+      "application" = "istio-base"
       "cluster"     = var.destination_cluster
     }, var.argocd_labels)
   }
@@ -92,7 +60,75 @@ resource "argocd_application" "this" {
 
     source {
       repo_url        = var.project_source_repo
-      path            = "charts/istio"
+      path            = "charts/istio-base"
+      target_revision = var.target_revision
+      helm {
+        values = data.utils_deep_merge_yaml.values.output
+      }
+    }
+
+    destination {
+      name      = var.destination_cluster
+      namespace = var.namespace
+    }
+
+    sync_policy {
+      dynamic "automated" {
+        for_each = toset(var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? [] : [var.app_autosync])
+        content {
+          prune       = automated.value.prune
+          self_heal   = automated.value.self_heal
+          allow_empty = automated.value.allow_empty
+        }
+      }
+
+      retry {
+        backoff {
+          duration     = "20s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+        limit = "5"
+      }
+
+      sync_options = [
+        "CreateNamespace=true",
+        "ServerSideApply=true",
+        "Replace=true",
+        "ApplyOutOfSyncOnly=true",
+        "RespectIgnoreDifferences=true"
+      ]
+    }
+  }
+
+  depends_on = [
+    resource.null_resource.dependencies,
+  ]
+}
+
+resource "argocd_application" "istiod" {
+  metadata {
+    name      = var.destination_cluster != "in-cluster" ? "istio-istiod-${var.destination_cluster}" : "istio-istiod"
+    namespace = var.argocd_namespace
+    labels = merge({
+      "application" = "istio-istiod"
+      "cluster"     = var.destination_cluster
+    }, var.argocd_labels)
+  }
+
+  timeouts {
+    create = "15m"
+    delete = "15m"
+  }
+
+  wait = var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? false : true
+
+  spec {
+    project = var.argocd_project == null ? argocd_project.this[0].metadata.0.name : var.argocd_project
+
+    source {
+      repo_url        = var.project_source_repo
+      path            = "charts/istio-istiod"
       target_revision = var.target_revision
       helm {
         values = data.utils_deep_merge_yaml.values.output
@@ -130,139 +166,75 @@ resource "argocd_application" "this" {
   }
 
   depends_on = [
-    resource.null_resource.dependencies,
+    resource.argocd_application.base
+  ]
+}
+resource "argocd_application" "gateway" {
+  metadata {
+    name      = var.destination_cluster != "in-cluster" ? "istio-gateway-${var.destination_cluster}" : "istio-gateway"
+    namespace = var.argocd_namespace
+    labels = merge({
+      "application" = "istio-gateway"
+      "cluster"     = var.destination_cluster
+    }, var.argocd_labels)
+  }
+
+  timeouts {
+    create = "15m"
+    delete = "15m"
+  }
+
+  wait = var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? false : true
+
+  spec {
+    project = var.argocd_project == null ? argocd_project.this[0].metadata.0.name : var.argocd_project
+
+    source {
+      repo_url        = var.project_source_repo
+      path            = "charts/istio-gateway"
+      target_revision = var.target_revision
+      helm {
+        values = data.utils_deep_merge_yaml.values.output
+      }
+    }
+
+    destination {
+      name      = var.destination_cluster
+      namespace = var.namespace
+    }
+
+    sync_policy {
+      dynamic "automated" {
+        for_each = toset(var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? [] : [var.app_autosync])
+        content {
+          prune       = automated.value.prune
+          self_heal   = automated.value.self_heal
+          allow_empty = automated.value.allow_empty
+        }
+      }
+
+      retry {
+        backoff {
+          duration     = "20s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+        limit = "5"
+      }
+
+      sync_options = [
+        "CreateNamespace=true"
+      ]
+    }
+  }
+
+  depends_on = [
+    resource.argocd_application.istiod
   ]
 }
 
-# resource "argocd_application" "istiod" {
-#   metadata {
-#     name      = var.destination_cluster != "in-cluster" ? "istio-istiod-${var.destination_cluster}" : "istio-istiod"
-#     namespace = var.argocd_namespace
-#     labels = merge({
-#       "application" = "istio-istiod"
-#       "cluster"     = var.destination_cluster
-#     }, var.argocd_labels)
-#   }
-
-#   timeouts {
-#     create = "15m"
-#     delete = "15m"
-#   }
-
-#   wait = var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? false : true
-
-#   spec {
-#     project = var.argocd_project == null ? argocd_project.this[0].metadata.0.name : var.argocd_project
-
-#     source {
-#       repo_url        = var.project_source_repo
-#       path            = "charts/istio-istiod"
-#       target_revision = var.target_revision
-#       helm {
-#         values = data.utils_deep_merge_yaml.values.output
-#       }
-#     }
-
-#     destination {
-#       name      = var.destination_cluster
-#       namespace = var.namespace
-#     }
-
-#     sync_policy {
-#       dynamic "automated" {
-#         for_each = toset(var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? [] : [var.app_autosync])
-#         content {
-#           prune       = automated.value.prune
-#           self_heal   = automated.value.self_heal
-#           allow_empty = automated.value.allow_empty
-#         }
-#       }
-
-#       retry {
-#         backoff {
-#           duration     = "20s"
-#           max_duration = "2m"
-#           factor       = "2"
-#         }
-#         limit = "5"
-#       }
-
-#       sync_options = [
-#         "CreateNamespace=true"
-#       ]
-#     }
-#   }
-
-#   depends_on = [
-#     resource.argocd_application.base
-#   ]
-# }
-# resource "argocd_application" "gateway" {
-#   metadata {
-#     name      = var.destination_cluster != "in-cluster" ? "istio-gateway-${var.destination_cluster}" : "istio-gateway"
-#     namespace = var.argocd_namespace
-#     labels = merge({
-#       "application" = "istio-gateway"
-#       "cluster"     = var.destination_cluster
-#     }, var.argocd_labels)
-#   }
-
-#   timeouts {
-#     create = "15m"
-#     delete = "15m"
-#   }
-
-#   wait = var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? false : true
-
-#   spec {
-#     project = argocd_project.gateway[0].metadata.0.name
-
-#     source {
-#       repo_url        = var.project_source_repo
-#       path            = "charts/istio-gateway"
-#       target_revision = var.target_revision
-#       helm {
-#         values = data.utils_deep_merge_yaml.values.output
-#       }
-#     }
-
-#     destination {
-#       name      = var.destination_cluster
-#       namespace = "istio-ingress"
-#     }
-
-#     sync_policy {
-#       dynamic "automated" {
-#         for_each = toset(var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? [] : [var.app_autosync])
-#         content {
-#           prune       = automated.value.prune
-#           self_heal   = automated.value.self_heal
-#           allow_empty = automated.value.allow_empty
-#         }
-#       }
-
-#       retry {
-#         backoff {
-#           duration     = "20s"
-#           max_duration = "2m"
-#           factor       = "2"
-#         }
-#         limit = "5"
-#       }
-
-#       sync_options = [
-#         "CreateNamespace=true"
-#       ]
-#     }
-#   }
-
-#   depends_on = [
-#     resource.argocd_application.istiod
-#   ]
-# }
-
 resource "null_resource" "this" {
   depends_on = [
-    resource.argocd_application.this,
+    resource.argocd_application.gateway,
   ]
 }
