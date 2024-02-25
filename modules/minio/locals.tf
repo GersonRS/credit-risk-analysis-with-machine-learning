b74eb53aa8853d7a46c93141bf46409b103ee695
GersonRS <gersonrodriguessantos8@gmail.com>
2024-02-25 01:10:53 -0300

    Feature: Add minio operator and tenant module

--- modules/minio/locals.tf
+++ modules/minio/locals.tf
@@ -1,23 +1,8 @@
-resource "random_password" "mlflow_secretkey" {
+resource "random_password" "minio_root_secretkey" {
   length  = 32
   special = false
 }
-resource "random_password" "airflow_secretkey" {
-  length  = 32
-  special = false
-}
-resource "random_password" "jupyterhub_secretkey" {
-  length  = 32
-  special = false
-}
-resource "random_password" "loki_secretkey" {
-  length  = 32
-  special = false
-}
-resource "random_password" "thanos_secretkey" {
-  length  = 32
-  special = false
-}
+
 locals {
   domain      = format("minio.apps.%s", var.base_domain)
   domain_full = format("minio.apps.%s.%s", var.cluster_name, var.base_domain)
@@ -60,6 +45,144 @@ locals {
   ) : null
 
   helm_values = [{
+    minio-operator = {
+      operator = {
+        replicaCount = 1
+        env = [
+          {
+            name  = "MINIO_IDENTITY_OPENID_CONFIG_URL"
+            value = "${var.oidc.issuer_url}/.well-known/openid-configuration"
+          },
+          {
+            name  = "MINIO_IDENTITY_OPENID_CLIENT_ID"
+            value = var.oidc.client_id
+          },
+          {
+            name  = "MINIO_IDENTITY_OPENID_CLIENT_SECRET"
+            value = var.oidc.client_secret
+          },
+          {
+            name  = "MINIO_IDENTITY_OPENID_CLAIM_NAME"
+            value = "policy"
+          },
+          {
+            name  = "MINIO_IDENTITY_OPENID_CLAIM_PREFIX"
+            value = ""
+          },
+          {
+            name  = "MINIO_IDENTITY_OPENID_SCOPES"
+            value = "openid,profile,email"
+          },
+          {
+            name  = "MINIO_IDENTITY_OPENID_REDIRECT_URI"
+            value = format("https://%s/oauth_callback", format("minio-console.apps.%s.%s", var.cluster_name, var.base_domain))
+          },
+          {
+            name  = "MINIO_IDENTITY_OPENID_COMMENT"
+            value = ""
+          },
+        ]
+      }
+      console = {
+        ingress = {
+          enabled          = true
+          ingressClassName = "traefik"
+          annotations = {
+            "cert-manager.io/cluster-issuer"                   = "${var.cluster_issuer}"
+            "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure"
+            "traefik.ingress.kubernetes.io/router.middlewares" = "traefik-withclustername@kubernetescrd"
+            "traefik.ingress.kubernetes.io/router.tls"         = "true"
+            "ingress.kubernetes.io/ssl-redirect"               = "true"
+            "kubernetes.io/ingress.allow-http"                 = "false"
+          }
+          tls = [{
+            secretName = "minio-operator-tls"
+            hosts      = [format("minio-console.apps.%s.%s", var.cluster_name, var.base_domain)]
+          }]
+          host = format("minio-console.apps.%s.%s", var.cluster_name, var.base_domain)
+        }
+      }
+    }
+    tenant = {
+      secrets = {
+        name      = "minio-env-configuration"
+        accessKey = "minio"
+        secretKey = "${random_password.minio_root_secretkey.result}"
+      }
+      tenant = {
+        name = "minio"
+        configuration = {
+          name = "minio-env-configuration"
+        }
+        pools = [{
+          servers          = 2
+          volumesPerServer = 2
+          size             = "10Gi"
+        }]
+        buckets = local.minio_config.buckets
+        users = [
+          {
+            CONSOLE_ACCESS_KEY = "airflow-user"
+            CONSOLE_SECRET_KEY = random_password.minio_root_secretkey.result
+          }
+        ]
+        env = [
+          {
+            name  = "MINIO_IDENTITY_OPENID_CONFIG_URL"
+            value = "${var.oidc.issuer_url}/.well-known/openid-configuration"
+          },
+          {
+            name  = "MINIO_IDENTITY_OPENID_CLIENT_ID"
+            value = var.oidc.client_id
+          },
+          {
+            name  = "MINIO_IDENTITY_OPENID_CLIENT_SECRET"
+            value = var.oidc.client_secret
+          },
+          {
+            name  = "MINIO_IDENTITY_OPENID_CLAIM_NAME"
+            value = "policy"
+          },
+          {
+            name  = "MINIO_IDENTITY_OPENID_CLAIM_PREFIX"
+            value = ""
+          },
+          {
+            name  = "MINIO_IDENTITY_OPENID_SCOPES"
+            value = "openid,profile,email"
+          },
+          {
+            name  = "MINIO_IDENTITY_OPENID_REDIRECT_URI"
+            value = format("https://%s/oauth_callback", format("minio-tenant.apps.%s.%s", var.cluster_name, var.base_domain))
+          },
+          {
+            name  = "MINIO_IDENTITY_OPENID_COMMENT"
+            value = ""
+          },
+        ]
+      }
+      ingress = {
+        console = {
+          enabled = true
+          ingressClassName : "traefik"
+          annotations = {
+            "cert-manager.io/cluster-issuer"                   = "${var.cluster_issuer}"
+            "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure"
+            "traefik.ingress.kubernetes.io/router.middlewares" = "traefik-withclustername@kubernetescrd"
+            "traefik.ingress.kubernetes.io/router.tls"         = "true"
+            "ingress.kubernetes.io/ssl-redirect"               = "true"
+            "kubernetes.io/ingress.allow-http"                 = "false"
+          }
+          tls = [{
+            secretName = "minio-tenant-tls"
+            hosts = [
+              format("minio-tenant.apps.%s.%s", var.cluster_name, var.base_domain),
+            ]
+          }]
+          host = format("minio-tenant.apps.%s.%s", var.cluster_name, var.base_domain)
+        }
+      }
+    }
     minio = merge(
       {
         mode = "standalone" # Set the deployment mode of MinIO to standalone
@@ -179,27 +302,27 @@ locals {
     users = [
       {
         accessKey = "mlflow-user"
-        secretKey = random_password.mlflow_secretkey.result
+        secretKey = random_password.minio_root_secretkey.result
         policy    = "mlflow-policy"
       },
       {
         accessKey = "airflow-user"
-        secretKey = random_password.airflow_secretkey.result
+        secretKey = random_password.minio_root_secretkey.result
         policy    = "airflow-policy"
       },
       {
         accessKey = "jupterhub-user"
-        secretKey = random_password.jupyterhub_secretkey.result
+        secretKey = random_password.minio_root_secretkey.result
         policy    = "jupterhub-policy"
       },
       {
         accessKey = "loki-user"
-        secretKey = random_password.loki_secretkey.result
+        secretKey = random_password.minio_root_secretkey.result
         policy    = "loki-policy"
       },
       {
         accessKey = "thanos-user"
-        secretKey = random_password.thanos_secretkey.result
+        secretKey = random_password.minio_root_secretkey.result
         policy    = "thanos-policy"
       }
     ],
--- modules/minio/main.tf
+++ modules/minio/main.tf
@@ -2,11 +2,6 @@ resource "null_resource" "dependencies" {
   triggers = var.dependency_ids
 }
 
-resource "random_password" "minio_root_secretkey" {
-  length  = 16
-  special = false
-}
-
 resource "kubernetes_service_account" "minio_storage" {
   metadata {
     name = "minio-storage"
@@ -80,6 +75,134 @@ resource "argocd_project" "this" {
 
 data "utils_deep_merge_yaml" "values" {
   input = [for i in concat(local.helm_values, var.helm_values) : yamlencode(i)]
+}
+
+resource "argocd_application" "operator" {
+  metadata {
+    name      = var.destination_cluster != "in-cluster" ? "minio-operator-${var.destination_cluster}" : "minio-operator"
+    namespace = var.argocd_namespace
+    labels = merge({
+      "application" = "minio-operator"
+      "cluster"     = var.destination_cluster
+    }, var.argocd_labels)
+  }
+
+  timeouts {
+    create = "15m"
+    delete = "15m"
+  }
+
+  wait = var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? false : true
+
+  spec {
+    project = var.argocd_project == null ? argocd_project.this[0].metadata.0.name : var.argocd_project
+
+    source {
+      repo_url        = var.project_source_repo
+      path            = "charts/minio-operator"
+      target_revision = var.target_revision
+      helm {
+        values = data.utils_deep_merge_yaml.values.output
+      }
+    }
+
+    destination {
+      name      = var.destination_cluster
+      namespace = var.namespace
+    }
+
+    sync_policy {
+      dynamic "automated" {
+        for_each = toset(var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? [] : [var.app_autosync])
+        content {
+          prune       = automated.value.prune
+          self_heal   = automated.value.self_heal
+          allow_empty = automated.value.allow_empty
+        }
+      }
+
+      retry {
+        backoff {
+          duration     = "20s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+        limit = "5"
+      }
+
+      sync_options = [
+        "CreateNamespace=true"
+      ]
+    }
+  }
+
+  depends_on = [
+    resource.null_resource.dependencies,
+  ]
+}
+
+resource "argocd_application" "tenant" {
+  metadata {
+    name      = var.destination_cluster != "in-cluster" ? "tenant-${var.destination_cluster}" : "tenant"
+    namespace = var.argocd_namespace
+    labels = merge({
+      "application" = "tenant"
+      "cluster"     = var.destination_cluster
+    }, var.argocd_labels)
+  }
+
+  timeouts {
+    create = "15m"
+    delete = "15m"
+  }
+
+  wait = var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? false : true
+
+  spec {
+    project = var.argocd_project == null ? argocd_project.this[0].metadata.0.name : var.argocd_project
+
+    source {
+      repo_url        = var.project_source_repo
+      path            = "charts/minio-tenant"
+      target_revision = var.target_revision
+      helm {
+        values = data.utils_deep_merge_yaml.values.output
+      }
+    }
+
+    destination {
+      name      = var.destination_cluster
+      namespace = var.namespace
+    }
+
+    sync_policy {
+      dynamic "automated" {
+        for_each = toset(var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? [] : [var.app_autosync])
+        content {
+          prune       = automated.value.prune
+          self_heal   = automated.value.self_heal
+          allow_empty = automated.value.allow_empty
+        }
+      }
+
+      retry {
+        backoff {
+          duration     = "20s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+        limit = "5"
+      }
+
+      sync_options = [
+        "CreateNamespace=true"
+      ]
+    }
+  }
+
+  depends_on = [
+    resource.argocd_application.operator,
+  ]
 }
 
 resource "argocd_application" "this" {
