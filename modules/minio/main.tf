5cf1424d21d2750b5eb8de11ef80723f2ea9adf3
GersonRS <gersonrodriguessantos8@gmail.com>
2024-02-25 02:25:53 -0300

    Feature: Add minio operator and tenant module

--- modules/minio/main.tf
+++ modules/minio/main.tf
@@ -79,6 +79,9 @@ data "utils_deep_merge_yaml" "values" {
 data "utils_deep_merge_yaml" "values_operator" {
   input = [for i in concat(local.helm_values_operator, var.helm_values) : yamlencode(i)]
 }
+data "utils_deep_merge_yaml" "values_tenant" {
+  input = [for i in concat(local.helm_values_tenant, var.helm_values) : yamlencode(i)]
+}
 
 resource "argocd_application" "operator" {
   metadata {
@@ -144,69 +147,69 @@ resource "argocd_application" "operator" {
   ]
 }
 
-# resource "argocd_application" "tenant" {
-#   metadata {
-#     name      = var.destination_cluster != "in-cluster" ? "tenant-${var.destination_cluster}" : "tenant"
-#     namespace = var.argocd_namespace
-#     labels = merge({
-#       "application" = "tenant"
-#       "cluster"     = var.destination_cluster
-#     }, var.argocd_labels)
-#   }
+resource "argocd_application" "tenant" {
+  metadata {
+    name      = var.destination_cluster != "in-cluster" ? "tenant-${var.destination_cluster}" : "tenant"
+    namespace = var.argocd_namespace
+    labels = merge({
+      "application" = "tenant"
+      "cluster"     = var.destination_cluster
+    }, var.argocd_labels)
+  }
 
-#   timeouts {
-#     create = "15m"
-#     delete = "15m"
-#   }
+  timeouts {
+    create = "15m"
+    delete = "15m"
+  }
 
-#   wait = var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? false : true
+  wait = var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? false : true
 
-#   spec {
-#     project = var.argocd_project == null ? argocd_project.this[0].metadata.0.name : var.argocd_project
+  spec {
+    project = var.argocd_project == null ? argocd_project.this[0].metadata.0.name : var.argocd_project
 
-#     source {
-#       repo_url        = var.project_source_repo
-#       path            = "charts/minio-tenant"
-#       target_revision = var.target_revision
-#       helm {
-#         values = data.utils_deep_merge_yaml.values.output
-#       }
-#     }
+    source {
+      repo_url        = var.project_source_repo
+      path            = "charts/minio-tenant"
+      target_revision = var.target_revision
+      helm {
+        values = data.utils_deep_merge_yaml.values_tenant.output
+      }
+    }
 
-#     destination {
-#       name      = var.destination_cluster
-#       namespace = var.namespace
-#     }
+    destination {
+      name      = var.destination_cluster
+      namespace = var.namespace
+    }
 
-#     sync_policy {
-#       dynamic "automated" {
-#         for_each = toset(var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? [] : [var.app_autosync])
-#         content {
-#           prune       = automated.value.prune
-#           self_heal   = automated.value.self_heal
-#           allow_empty = automated.value.allow_empty
-#         }
-#       }
+    sync_policy {
+      dynamic "automated" {
+        for_each = toset(var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? [] : [var.app_autosync])
+        content {
+          prune       = automated.value.prune
+          self_heal   = automated.value.self_heal
+          allow_empty = automated.value.allow_empty
+        }
+      }
 
-#       retry {
-#         backoff {
-#           duration     = "20s"
-#           max_duration = "2m"
-#           factor       = "2"
-#         }
-#         limit = "5"
-#       }
+      retry {
+        backoff {
+          duration     = "20s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+        limit = "5"
+      }
 
-#       sync_options = [
-#         "CreateNamespace=true"
-#       ]
-#     }
-#   }
+      sync_options = [
+        "CreateNamespace=true"
+      ]
+    }
+  }
 
-#   depends_on = [
-#     resource.argocd_application.operator,
-#   ]
-# }
+  depends_on = [
+    resource.argocd_application.operator,
+  ]
+}
 
 # resource "argocd_application" "this" {
 #   metadata {
@@ -274,7 +277,7 @@ resource "argocd_application" "operator" {
 
 resource "null_resource" "this" {
   depends_on = [
-    resource.argocd_application.operator,
+    resource.argocd_application.tenant,
   ]
 }
 
