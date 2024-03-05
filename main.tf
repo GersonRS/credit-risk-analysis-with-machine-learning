5dbeb36f855b6cd85ee441114845aeb64d0bdbb7
GersonRS <gersonrodriguessantos8@gmail.com>
2024-03-05 15:47:51 -0300

    Fix: Adjust keycloa chart

--- main.tf
+++ main.tf
@@ -64,19 +64,19 @@ module "kserve" {
   }
 }
 
-# module "keycloak" {
-#   source              = "./modules/keycloak"
-#   cluster_name        = local.cluster_name
-#   base_domain         = local.base_domain
-#   cluster_issuer      = local.cluster_issuer
-#   argocd_namespace    = module.argocd_bootstrap.argocd_namespace
-#   target_revision     = local.target_revision
-#   project_source_repo = local.project_source_repo
-#   dependency_ids = {
-#     traefik      = module.traefik.id
-#     cert-manager = module.cert-manager.id
-#   }
-# }
+module "keycloak" {
+  source              = "./modules/keycloak"
+  cluster_name        = local.cluster_name
+  base_domain         = local.base_domain
+  cluster_issuer      = local.cluster_issuer
+  argocd_namespace    = module.argocd_bootstrap.argocd_namespace
+  target_revision     = local.target_revision
+  project_source_repo = local.project_source_repo
+  dependency_ids = {
+    istio        = module.istio.id
+    cert-manager = module.cert-manager.id
+  }
+}
 
 # module "oidc" {
 #   source              = "./modules/oidc"
