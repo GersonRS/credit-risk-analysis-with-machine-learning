b0b32b70c48fd3b618c8508dbd3cabdaff16344e
GersonRS <gersonrodriguessantos8@gmail.com>
2024-03-13 18:21:36 -0300

    Feature: Add nifi ingress

--- modules/nifi/locals.tf
+++ modules/nifi/locals.tf
@@ -1,9 +1,39 @@
 locals {
   helm_values = [{
-    oidc = {
-      url           = "${var.oidc.issuer_url}/.well-known/openid-configuration"
-      client_id     = var.oidc.client_id
-      client_secret = var.oidc.client_secret
+    nifi = {
+      oidc = {
+        url           = "${var.oidc.issuer_url}/.well-known/openid-configuration"
+        client_id     = var.oidc.client_id
+        client_secret = var.oidc.client_secret
+      }
+      ingress = {
+        enabled = true
+        annotations = {
+          "cert-manager.io/cluster-issuer"                   = "${var.cluster_issuer}"
+          "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure"
+          "traefik.ingress.kubernetes.io/router.middlewares" = "traefik-withclustername@kubernetescrd"
+          "traefik.ingress.kubernetes.io/router.tls"         = "true"
+          "ingress.kubernetes.io/ssl-redirect"               = "true"
+          "kubernetes.io/ingress.allow-http"                 = "false"
+        }
+        hosts = [
+          {
+            host = "nifi.apps.${var.base_domain}"
+            path = "/"
+          },
+          {
+            host = "nifi.apps.${var.cluster_name}.${var.base_domain}"
+            path = "/"
+          },
+        ]
+        tls = [{
+          secretName = "nifi-tls"
+          hosts = [
+            "nifi.apps.${var.base_domain}",
+            "nifi.apps.${var.cluster_name}.${var.base_domain}"
+          ]
+        }]
+      }
     }
     nifikop = {
       image = {
