d1678e5c72a95c3d38b87998f8c2eb48db742970
GersonRS <gersonrodriguessantos8@gmail.com>
2024-03-13 19:41:58 -0300

    Feature: try again

--- modules/nifi/locals.tf
+++ modules/nifi/locals.tf
@@ -2,7 +2,7 @@ locals {
   helm_values = [{
     nifi = {
       oidc = {
-        url           = "${var.oidc.issuer_url}/.well-known/openid-configuration"
+        url           = "keycloak.keycloak.svc.cluster.local/realms/modern-gitops-stac/.well-known/openid-configuration"
         client_id     = var.oidc.client_id
         client_secret = var.oidc.client_secret
       }
